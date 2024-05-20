ff68fb9dfc23aa78515c967ae3489784bb5828b8
Peolahh <104804036+Peolahh@users.noreply.github.com>
2024-05-20 11:44:58 -0300

    version 1.1

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
-import {  authUser,
+import {  
+    authUser,
     registerUser,
     logoutUser,
     getUserProfile,
@@ -11,7 +12,7 @@ import {  authUser,
     updateUser,
 } from '../controllers/userController.js';
 
-router.route('/').post.apply(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
